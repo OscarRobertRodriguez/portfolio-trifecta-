576a6626e0b9511d94903da3f8fec0ebdd384aa9
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2018-01-15 17:45:26 -0600

    aligned index.js code

--- dev/index.js
+++ dev/index.js
@@ -10,19 +10,19 @@ require('images/puzzle.jpg');
 
 
 
-const myPhotoTag = document.querySelector(".me-photo"),
-    octocatIconTags = [...document.querySelectorAll(".octocat-icon")],
-    launchIconTags = [...document.querySelectorAll(".launch-icon")],
+const myPhotoTag        = document.querySelector(".me-photo"),
+    octocatIconTags     = [...document.querySelectorAll(".octocat-icon")],
+    launchIconTags      = [...document.querySelectorAll(".launch-icon")],
     contactCardIconTags = [...document.querySelectorAll('.contact-card__front-side__links__link__icon')],
-    flipTagFront = document.querySelector('.contact-card__front-side__flip-btn__img'),
-    flipTagBack = document.querySelector('.contact-card__back-side__flip-btn__img'),
-    backSide = document.querySelector('.contact-card__back-side');
+    flipTagFront        = document.querySelector('.contact-card__front-side__flip-btn__img'),
+    flipTagBack         = document.querySelector('.contact-card__back-side__flip-btn__img'),
+    backSide            = document.querySelector('.contact-card__back-side');
 
 // add github icons to all locations
 octocatIconTags.forEach(icon => icon.src = require("images/github.png"));
-launchIconTags.forEach(icon => icon.src = require("images/launch-icon.png"));
-flipTagFront.src = require('images/flip.png');
-flipTagBack.src = require('images/flip.png');
+launchIconTags.forEach(icon  => icon.src = require("images/launch-icon.png"));
+flipTagFront.src              = require('images/flip.png');
+flipTagBack.src               = require('images/flip.png');
 
 
 // add icon link images for contact card
@@ -41,7 +41,7 @@ const accordions = document.querySelectorAll(".accordion-title");
 
 //event listener to open and close mobile menu when hamburger is clicked
 
-[...accordions].forEach(accordion =>
+[...accordions].forEach(accordion => 
     accordion.addEventListener("click", openAndCloseAccordion)
 );
 
